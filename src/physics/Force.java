ac6e22b5a29cc4f9ccb12543a8a6c5f69be8bea1
Pritam Mathivanan <Prit@wl-10-190-54-139.wireless.duke.edu>
2014-02-01 19:10:14 -0500

    created vector

---
+++ src/physics/Force.java
@@ -0,0 +1,18 @@
+package physics;
+
+public abstract class Force {
+	
+	private static final int distance = 50;
+	private double magnitude;
+	private double exponent;
+
+	public Force (double mag, double exp){
+		magnitude = mag;
+		exponent = exp;
+	}
+	
+	public Vector createForce (double ang, double length) {
+		Vector 
+	}
+	
+}
---
+++ src/physics/Vector.java
@@ -0,0 +1,30 @@
+package physics;
+
+public class Vector {
+	
+	private double angle;
+	private double magnitude;
+	
+	public Vector(){
+		this(0,0);
+	}
+	
+	public Vector (double ang, double mag){
+		setAngle(ang);
+		setMagnitude(mag);
+	}
+	
+	public void reset() {
+		setAngle(0);
+		setMagnitude(0);
+	}
+	
+	protected void setAngle(double val){
+		angle = val;
+	}
+	
+	protected void setMagnitude(double val){
+		magnitude = val;
+	}
+
+}
